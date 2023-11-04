b9f732565736f72124c9b0a230be7116fb037b5b
Usha Lingamallu <lingamalluusha@gmail.com>
2023-11-04 08:43:17 +0100

    headings

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { HappyThoughts } from "./components/HappyThoughts";
 export const App = () => {
   return (
     <div>
+      <h1> Project Happy Thoughts </h1>
+      <h2 className="technigoheading"> Technigo Education Team</h2>
       <HappyThoughts />
     </div>
   );
